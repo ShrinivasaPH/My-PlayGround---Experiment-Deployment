d8e6a05978dd015a90536f4530e6267f3a95ddf6
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2025-02-07 22:45:09 +0530

    Update playground.py

--- playground.py
+++ playground.py
@@ -13,7 +13,7 @@ with open("gmm_model.pkl", "rb") as f:
 
 # Streamlit UI
 st.title("Gaussian Mixture Model Prediction App")
-st.write("Enter feature values to predict the cluster.")
+st.header("Enter feature values to predict the cluster.")
 
 # User input fields
 feature1 = st.slider("Feature 1", min_value=-10.0, max_value=10.0, step=0.1)
@@ -30,6 +30,9 @@ if st.button("Predict Cluster"):
     cluster = gmm.predict(user_data_scaled)[0]
     st.success(f"The predicted cluster is: **{cluster}**")
 
+
 st.divider()
 
-st.image("Cluster image.png", caption="The Clusters")
+#st.image("Cluster image.png", caption="The Clusters")
+#st.balloons()
+#st.snow()
